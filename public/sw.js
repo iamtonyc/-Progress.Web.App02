dca7524e11f0ef0f14a61cf0e90e2ba28f31b302
Tony <iamtonycheung@gmail.com>
2022-01-19 16:10:37 +0800

    up to lesson 65

--- public/sw.js
+++ public/sw.js
@@ -14,7 +14,7 @@ self.addEventListener('activate', function(event) {
   console.log('[Service Worker] Activating Service Worker ....', event);
   return self.clients.claim();
 });
-
+ 
 self.addEventListener('fetch', function(event) {
   event.respondWith(fetch(event.request));
 });
